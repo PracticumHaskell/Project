47b0f4c8fd4d0cbbba86acdb9558ef829bb688da
Natachiazza <142330342+Natachiazza@users.noreply.github.com>
2025-04-28 21:37:12 +0300

    Update Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -10,7 +10,7 @@ main = withCP65001 $ do
     enc <- getLocaleEncoding
     print enc
     setLocaleEncoding utf8
-    args <- getArgs -- IO [String]
+    args <- getArgs
     case args of
         [itemsFile, cartFile] -> runProgram itemsFile cartFile Nothing
         [itemsFile, cartFile, bonusFile] -> runProgram itemsFile cartFile (Just bonusFile)
