e94a68738f0d3435566b34cfac2dc0ab532df2d7
Natachiazza <142330342+Natachiazza@users.noreply.github.com>
2025-04-30 13:09:12 +0300

    Update Lib.hs

--- src/Lib.hs
+++ src/Lib.hs
@@ -20,27 +20,6 @@ import System.Environment ()
 import Data.List.Split
 
 
-newtype Name = Name String deriving (Show, Read, Eq)
-newtype Price = Price Float deriving (Show, Read, Eq)
-newtype Category = Category String deriving (Show, Read, Eq)
-
-data Item = Item {
-    itemName     :: Name,
-    itemPrice    :: Price,
-    itemCategory :: Category
-} deriving (Show, Read, Eq)
-
--- Корзина
-data Cart = Cart {
-    cartItems :: [(Item, Int)]
-} deriving (Show, Read)
-
--- Бонусная карта
-data BonusCard = BonusCard {
-    birthDate :: (Int, Int, Int),  -- (год, месяц, день)
-    basediscount  :: Float  -- скидка до 7%
-} deriving (Show, Read)
-
 -- стоимость корзины без скидки
 cartTotal :: Cart -> Float
 cartTotal (Cart items) = sum [ getPrice item * fromIntegral qty |(item, qty) <- items ]
